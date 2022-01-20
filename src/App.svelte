a6935ff2be2e0aa73af8004826e65cf0bff8b477
Scott Upton <scott.upton@pnnl.gov>
2022-01-19 16:29:24 -0800

    Trying again

--- src/App.svelte
+++ src/App.svelte
@@ -25,7 +25,7 @@
   import { writable } from "svelte/store";
   const store = writable([]);
   $store.length; // incorrect no-unsafe-member-access error
-  console.log("Test2");
+  console.log("Test3");
   export let title: string;
 </script>
 
