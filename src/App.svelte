7a199905b49f9cf9352707e1ba206b913cfe6cde
Scott Upton <scott.upton@pnnl.gov>
2022-01-19 16:18:21 -0800

    Testing commit hook again

--- src/App.svelte
+++ src/App.svelte
@@ -25,7 +25,7 @@
   import { writable } from "svelte/store";
   const store = writable([]);
   $store.length; // incorrect no-unsafe-member-access error
-  console.log("Test");
+  console.log("Test2");
   export let title: string;
 </script>
 
