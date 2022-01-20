d97f61cfc00ce5a7fafe930b8b70ba7b5c2f2e9a
Scott Upton <scott.upton@pnnl.gov>
2022-01-19 16:03:53 -0800

    Testing pre commit hook

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
+
+<!-- App.svelte -->
+<script lang="ts">
+  import { writable } from "svelte/store";
+  const store = writable([]);
+  $store.length; // incorrect no-unsafe-member-access error
+  console.log("Test");
+  export let title: string;
+</script>
+
+<main>
+  <h1>Welcome to {title}!</h1>
+</main>
