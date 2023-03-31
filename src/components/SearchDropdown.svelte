ddd58d3963794eb97b105fdaaadbf959a045373f
PNL\buts930 <nathan.butschli@pnnl.gov>
2023-03-31 15:26:00 -0700

    -Removed ID filtering from search. -Added CAS Number, Chemical Formula, and UN Number to search results and recently viewed. -Added bold formatting to search specific matching search parameter

--- src/components/SearchDropdown.svelte
+++ src/components/SearchDropdown.svelte
@@ -134,14 +134,6 @@
           .includes(searchBarFilter.toLowerCase().trim())
     );
 
-    const foundID = items.filter(
-      currItem =>
-        currItem.Chemical_ID &&
-        currItem.Chemical_ID.toLowerCase()
-          .trim()
-          .includes(searchBarFilter.toLowerCase().trim())
-    );
-
     if (foundCas.length > 0) {
       filteredItems.set("foundCas", { label: "Matching CAS Number", items: foundCas });
     }
@@ -162,9 +154,6 @@
         label: "Matching UN Number",
         items: foundUN_Number
       });
-    }
-    if (foundID.length > 0) {
-      filteredItems.set("foundID", { label: "Matching ID", items: foundID });
     }
     if (filteredItems.size === 0) {
       filteredItems.set("noneFound", { label: "No Matches" });
@@ -238,7 +227,28 @@
         {#if value.items}
           {#each value.items as item}
             <div class="search-item" on:click={() => handleSearchItemSelect(item)}>
-              {item.Chemical_Name}
+              {#if key == "foundCas"}
+                <b>{item.CAS_Number}</b>
+              {:else}
+                {item.CAS_Number}
+              {/if}
+              {#if key == "foundName"}
+                <b>{item.Chemical_Name}</b>
+              {:else}
+                {item.Chemical_Name}
+              {/if}
+              {#if key == "foundFormula"}
+                <b>{item.Chemical_Formula}</b>
+              {:else}
+                {item.Chemical_Formula}
+              {/if}
+              {#if item.UN_Number != '<BR>'}
+                {#if key == "foundUN_Number"}
+                  <b>{item.UN_Number}</b>
+                {:else}
+                  {item.UN_Number}
+                {/if}
+              {/if}             
             </div>
           {/each}
         {/if}
